0e57aefe51fba07f81df4cc8b78bbb3f79d0503f
Marvin Beckers <mail@embik.me>
2023-11-25 10:28:06 +0100

    cmd: make aliases discoverable/visible
    Signed-off-by: Marvin Beckers <mail@embik.me>

--- src/cmd/version.rs
+++ src/cmd/version.rs
@@ -6,10 +6,10 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const NAME: &str = "version";
 
 pub fn command() -> Command {
-    Command::new(NAME).alias("v").about("Print version")
+    Command::new(NAME).visible_alias("v").about("Print version")
 }
 
 pub fn execute() -> Result<()> {
-    println!("{VERSION}");
+    println!("v{VERSION}");
     Ok(())
 }
